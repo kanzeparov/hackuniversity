c4f7a9c449556e5977f3b7f6da60f4df0fbc1122
Kanzeparov <kanzeparov@yandex.ru>
2019-03-23 14:55:36 +0300

    added android

---
+++ android/app/src/main/java/come/manager/direct/hackuniversity/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package come.manager.direct.hackuniversity;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+}

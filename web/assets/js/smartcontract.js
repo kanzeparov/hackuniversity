7264bbd360bd5fdbe76f6e0147b90a90a9ea7017
Иван <if1242@yandex.ru>
2019-03-24 11:20:48 +0300

    Release

--- web/assets/js/smartcontract.js
+++ web/assets/js/smartcontract.js
@@ -15,8 +15,6 @@ function deployConract(ticketName, ticketPrice, ticketOwner, ticketPersonInfo, t
             console.log(e, contract);
             if (typeof contract.address !== 'undefined') {
                 console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-                //productAddreses[step] = contract.address;
-                //step++;
                 var contract_address = contract.address;
 		        console.log('Address on JS before: ' + contract.address);
 			    var owner_address = ticketOwner;
@@ -36,8 +34,6 @@ function deployConract(ticketName, ticketPrice, ticketOwner, ticketPersonInfo, t
 
 function openContract(address){
     let contract = createContract();
-    //let index = productNames.indexOf(selectedValue);
-    //let address = productAddreses[index];
     let contractInstance = contract.at(address);
 
     return contractInstance;
@@ -51,62 +47,60 @@ function changeFunc() {
 }
 
 function getStates(contract, address) {
+	console.log(address); 
     console.log(contract);
-    //let index = productNames.indexOf(selectedValue);
-    //let address = productAddreses[index];
-	contract.Is_active.call({ from: window.web3.eth.accounts[0] },function (e,r) { 
+	step = 1; 
+	console.log(step++); 
+	contract.Is_active({ from: window.web3.eth.accounts[0] },function (e,r) { 
+	  console.log(step++); 
 	  console.log(r);
-	  
+	  console.log(address+'---open'); 
+	  if (r) {
+		document.getElementById(address+'---open').style.display = 'block';
+        document.getElementById(address+'---close').style.display = 'none';
+	  } else {
+		document.getElementById(address+'---open').style.display = 'none';
+        document.getElementById(address+'---close').style.display = 'block';
+
+	  }	
 	})
-    //contract.State.call({ from: window.web3.eth.accounts[0] },function (e,r) {
-        //console.log(r);
-        //console.log(r[1]);
-        //let index1 = r[1].indexOf(':')
-        //let index2= r[1].indexOf(':',index1+1)
-        //let container = r[1].substr(index1+1,index2-index1-1);
-        //let navi = r[1].substr(index2+1,6);
-        //console.log(container);
-        //console.log(navi);
-        //getNavi(container, navi);
-    //})
+	console.log(step++); 
+	
 }
 
 
 function open_ticket(contract, address) {
     console.log(contract);
-    //let index = productNames.indexOf(selectedValue);
-    //let address = productAddreses[index];
 	contract.Open({ from: window.web3.eth.accounts[0] },function (e,r) { 
 	  console.log(r);
 	  
 	})
-    //contract.State.call({ from: window.web3.eth.accounts[0] },function (e,r) {
-        //console.log(r);
-        //console.log(r[1]);
-        //let index1 = r[1].indexOf(':')
-        //let index2= r[1].indexOf(':',index1+1)
-        //let container = r[1].substr(index1+1,index2-index1-1);
-        //let navi = r[1].substr(index2+1,6);
-        //console.log(container);
-        //console.log(navi);
-        //getNavi(container, navi);
-    //})
 }
 
 function delegate (contract, address) {
 	let addr_to = '0xf6f45356002Eee48a0333c480da441dAdFcE1373';
 	let person = "Ruslan";
     console.log(contract);
-    //let index = productNames.indexOf(selectedValue);
-    //let address = productAddreses[index];
 	if (web3.eth.accounts[0]) {
     console.log(web3.eth.accounts[0]);
-  } else {
+    } else {
     setTimeout(loop, 100);
-  }
+    }
 	contract.Delegate(addr_to, person, { from: window.web3.eth.accounts[0] }, function (e,r) { 
 	 console.log(r);
-	})
+	 jQuery.ajax({
+	  type: "POST",
+	  url: "delegate.php",
+	  data: {contract_address : address, owner_address : addr_to},
+	  success: function(responce) {
+	  alert("Билет делегирован!")
+	}
+	});
+	 
+	 
+    })
+	
+	
     //contract.State.call({ from: window.web3.eth.accounts[0] },function (e,r) {
         //console.log(r);
         //console.log(r[1]);
